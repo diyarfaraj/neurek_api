fe797f515781f441d463bdf7ebd8951ca7c99ee4
Diyar Faraj <diyar.faraj@gmail.com>
2021-10-09 21:43:11 +0200

    added emoty models

---
+++ Entities/CandidateBlockedCompanies.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace neurek.Entities
+{
+    public class CandidateBlockedCompanies
+    {
+    }
+}
