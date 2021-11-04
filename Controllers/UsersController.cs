fe231ec6b799b827bdaf726bff3bd5853a343b77
Diyar Faraj <diyar.faraj@gmail.com>
2021-11-04 22:18:27 +0100

    added photo service

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,10 +1,12 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using neurek.Data;
 using neurek.DTOs;
 using neurek.Entities;
+using neurek.Extensions;
 using neurek.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -20,11 +22,13 @@ namespace neurek.Controllers
     {
         private readonly IUserRepository _userRepository;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository userRepository, IMapper mapper)
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _photoService = photoService;
         }
 
         [HttpGet]
@@ -33,7 +37,7 @@ namespace neurek.Controllers
             var users = await _userRepository.GetCandidatesAsync();
             return Ok(users);
         }
-        [HttpGet("{email}")]
+        [HttpGet("{email}", Name ="GetUser")]
         public async Task<ActionResult<CandidateDto>> GetUser( string email)
         {
             return await _userRepository.GetCandidateAsync(email);
@@ -49,13 +53,39 @@ namespace neurek.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateCandidate(CandidateUpdateDto candidateUpdateDto)
         {
-            var email = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var candidate = await _userRepository.GetUserByEmailAsync(email);
+            var candidate = await _userRepository.GetUserByEmailAsync(User.GetUserEmail());
 
             _mapper.Map(candidateUpdateDto, candidate);
             _userRepository.Update(candidate);
             if (await _userRepository.SaveAllAsync()) return NoContent();
             return BadRequest("failed to update user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepository.GetUserByEmailAsync(User.GetUserEmail());
+            var result = await _photoService.AddPhotoAsync(file);
+            if (result.Error != null) return BadRequest(result.Error.Message);
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            if(user.Photos.Count == 0)
+            {
+                photo.IsMain = true;
+            }
+
+            user.Photos.Add(photo);
+            if (await _userRepository.SaveAllAsync())
+            {
+                return CreatedAtRoute("GetUser", new { email = user.Email},_mapper.Map<PhotoDto>(photo));
+
+            }
+
+            return BadRequest("problem adding photo");
         }
     }
 }
