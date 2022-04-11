b00bf55271161cc4df3954487f0b05d66c134656
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-11 23:57:21 +0200

    add jobad repo

---
+++ Data/JobAdRepository.cs
@@ -0,0 +1,27 @@
+﻿using neurek.Entities;
+using neurek.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace neurek.Data
+{
+    public class JobAdRepository : IJobAdRepository
+    {
+        public Task<IEnumerable<JobAd>> GetJobAdsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(JobAd jobAd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/IJobAdRepository.cs
@@ -0,0 +1,15 @@
+﻿using neurek.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace neurek.Interfaces
+{
+    public interface IJobAdRepository
+    {
+        void Update(JobAd jobAd);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<JobAd>> GetJobAdsAsync();
+    }
+}
